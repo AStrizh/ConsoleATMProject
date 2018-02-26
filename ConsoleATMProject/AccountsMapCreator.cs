94741da9d242d3a153e6ed40f210ca0eb516dc3f
Aleksandr Strizhevskiy <abstrizhevskiy@gmail.com>
2018-02-26 05:18:48 -0500

    Successfully added a map creator class

---
+++ ConsoleATMProject/AccountsMapCreator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleATMProject
+{
+    class AccountsMapCreator
+    {
+
+        public static Dictionary<string, string[]> CreateMap(string filename)
+        {
+            StreamReader file = new StreamReader(filename);
+
+            Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+            string line = file.ReadLine();
+            string[] accountData;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                
+                accountData = line.Split(',');
+                dictionary.Add(accountData[1], accountData);
+
+                //string result = string.Join("    ", accountData);
+                //Console.WriteLine(result);
+            }
+
+            return dictionary;
+
+        }
+
+    }
+}
--- ConsoleATMProject/Launcher.cs
+++ ConsoleATMProject/Launcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleATMProject
 {
@@ -10,6 +11,36 @@ namespace ConsoleATMProject
     {
         static void Main(string[] args)
         {
+            //TODO: Figure out how to use working directory rather than hardcoded path
+            string myDirectory = "C:\\Users\\abstr\\Documents\\C#\\Group Project\\ConsoleATMProject\\ConsoleATMProject";
+            Dictionary<string, string[]> myDictionary = AccountsMapCreator.CreateMap(myDirectory +"\\accounts.csv");
+
+            Console.WriteLine("\nEnter the account number you are interested in: ");
+            string accountNumber = Console.ReadLine();
+
+            // See whether Dictionary contains this string.
+            if (myDictionary.ContainsKey(accountNumber))
+            {
+                string[] value = myDictionary[accountNumber];
+                string name = string.Join(" ", value[0].Split('.'));
+
+                Console.WriteLine("\nThe name on the account is: " + name);
+                Console.WriteLine("\nThe balance is: $" + value[2]);
+
+                if(String.Compare(value[3], "y") == 0)
+                {
+                    Console.WriteLine("\nThis is an Executive account!");
+                }
+                else
+                {
+                    Console.WriteLine("\nYou are a poor shmuck!");
+                }
+
+                Console.WriteLine("\nYour pin number is: " + value[4] + " Dont tell anybody!");
+            }
+            else
+                Console.WriteLine("\nThat account number doesnt exist");
+
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
         }
